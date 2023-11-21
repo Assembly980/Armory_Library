2f981382ddd7d70ec8148dd0ca8f219be8088202
TheGoose0507 <147533356+TheGoose0507@users.noreply.github.com>
2023-11-21 13:48:25 -0500

    Add files via upload

--- Weapons_API_WIP.py
+++ Weapons_API_WIP.py
@@ -2,30 +2,31 @@ from flask import Flask, jsonify
 import sqlite3
 from contextlib import closing
 
-app= Flask(__Weapons__)
+app= Flask(__name__)
 
 #Connect to SQLite
 def DBConnection():
-    return sqlite3.connect('Weapons.db')
+    #file path to the database
+    return sqlite3.connect('C:/Users/trevo/OneDrive/ServerSide/Weapons.db')
 
 #Request value of all swords
 def GetSwordsFromDB():
     with closing(DBConnection()) as connection, connection.cursor() as cursor:
-        cursor.execute('SELECT WeaponName, WeaponRarity, Descritpion, Picture FROM swords')
+        cursor.execute('SELECT WeaponName, WeaponRarity, Description, Picture FROM swords')
         swords = [{'WeaponName': WeaponName, 'WeaponRarity': WeaponRarity, 'Description': Description, 'Picture': Picture} for WeaponName, WeaponRarity, Description, Picture in cursor.fetchall()]
 
     return swords
 
 #Request value of individual sword
-def GetSwordFromDB(WeaponName)
+def GetSwordFromDB(WeaponName):
     with closing(DBConnection()) as connection, connection.cursor() as cursor:
-        cursor.execute('SELECT WeaponName, WeaponRarity, Description, Picture FROM swords WHERE name= ?', (WeaponName))
+        cursor.execute('SELECT WeaponName, WeaponRarity, Description, Picture FROM swords WHERE WeaponName = ?', (WeaponName,))
         RequestedSword = cursor.fetchone()
 
     return RequestedSword
 
 #returns all swords in database
-@app.route('api/swords', methods=['GET'])
+@app.route('/api/swords', methods=['GET'])
 def GetAllSwords():
     swords = GetSwordsFromDB()
     return jsonify(swords)
@@ -40,6 +41,6 @@ def GetRequestedSword(WeaponName):
     else:
         return jsonify(message='Sword not found! Its dangerous to go alone, take this wooden sword!'), 404
     
-if __Weapons__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
     
